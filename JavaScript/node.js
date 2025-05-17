3e12aebdfb67241766dc42b40498f99ae664c427
Ajay joseph <aj2506039@gmail.com>
2025-05-17 15:43:43 +0530

    node updated

---
+++ JavaScript/node.js
@@ -0,0 +1,9 @@
+// const http=require('http')
+
+// http.createServer((req,res)=>{
+//     res.write('hii form server')
+//     res.end('server created')
+// }).listen(3000,()=>{
+//     console.log('server creeated')
+// })
+
